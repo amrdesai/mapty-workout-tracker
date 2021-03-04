8359aea6100fe734a463b647066e1a9996f41a40
Aamer Desai <56724460+minm333@users.noreply.github.com>
2021-03-04 17:22:13 -0500

    Display workout input form

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+let map, mapEvent;
 
 // Get users loacation
 if (navigator.geolocation) {
@@ -20,8 +21,9 @@ if (navigator.geolocation) {
             const coords = [latitude, longitude];
 
             // Leaflet JS library - Map
-            const map = L.map('map').setView(coords, 13);
+            map = L.map('map').setView(coords, 13);
 
+            // Map as per current location
             L.tileLayer(
                 'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
                 {
@@ -30,13 +32,47 @@ if (navigator.geolocation) {
                 }
             ).addTo(map);
 
-            L.marker(coords)
-                .addTo(map)
-                .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-                .openPopup();
+            // Click Event to add a new workout
+            map.on('click', (mapEv) => {
+                mapEvent = mapEv;
+                form.classList.remove('hidden');
+                inputDistance.focus();
+            });
         },
         () => {
             alert(`Could not fetch user's location`);
         }
     );
 }
+
+// Event Listeners //
+// Event Listener: Form submit event
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Clear input fields
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value =
+        '';
+
+    // Display marker
+    const { lat, lng } = mapEvent.latlng;
+    L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+            L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup',
+            })
+        )
+        .setPopupContent('Workout')
+        .openPopup();
+});
+
+// Event Listener: Workout type change event
+inputType.addEventListener('change', (e) => {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
